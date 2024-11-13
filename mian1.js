6734880bff03920b9dbe687840ec34188c68aaf3
anit tech <anittech@gmail.com>
2024-11-13 22:35:26 +0530

    wehfghff

--- mian1.js
+++ mian1.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // // cart open close
 // let cartIcon = document.querySelector("#cart-icon");
 // let cart=document.querySelector(".cart");
@@ -9,4 +10,17 @@
 // //close cart
 // closeCart.onclick = () => {
 //     cart.classList.remove("active");
+=======
+// // cart open close
+// let cartIcon = document.querySelector("#cart-icon");
+// let cart=document.querySelector(".cart");
+// let closeCart=document.querySelector("#close-cart");
+// //open cart
+// cartIcon.onclick = () => {
+//     cart.classList.add("active");
+// }
+// //close cart
+// closeCart.onclick = () => {
+//     cart.classList.remove("active");
+>>>>>>> bd5ec25 (hfghff)
 // }   
